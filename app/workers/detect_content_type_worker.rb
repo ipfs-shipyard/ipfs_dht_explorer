c2a08a771e480bfaec7d0c73bdcc901cb6858ec5
Andrew Nesbitt <andrewnez@gmail.com>
2020-12-01 11:41:17 +0000

    Add different levels of priority queues in sidekiq

--- app/workers/detect_content_type_worker.rb
+++ app/workers/detect_content_type_worker.rb
@@ -1,5 +1,6 @@
 class DetectContentTypeWorker
   include Sidekiq::Worker
+  sidekiq_options queue: 'low'
 
   def perform(cid_id)
     Cid.find_by_id(cid_id).try(:detect_content_type)
--- app/workers/manual_crawl_worker.rb
+++ app/workers/manual_crawl_worker.rb
@@ -1,5 +1,6 @@
 class ManualCrawlWorker
   include Sidekiq::Worker
+  sidekiq_options queue: 'low'
 
   def perform(node_id)
     Node.find_by_id(node_id).try(:manual_crawl)
