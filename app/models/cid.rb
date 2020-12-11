a0e6ec4886cc565a08bb5e2b63c243b384df9cac
Andrew Nesbitt <andrewnez@gmail.com>
2020-12-11 12:07:38 +0000

    CID exporting

--- app/models/cid.rb
+++ app/models/cid.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Cid < ApplicationRecord
   has_many :wants, dependent: :delete_all
   has_many :nodes, through: :wants
@@ -17,5 +19,29 @@ class Cid < ApplicationRecord
       # failed to find
       update_column(:last_loaded_at, Time.now)
     end
+  end
+
+  def self.export(path, wants_count)
+    scope = Cid.where('wants_count > ?', wants_count)
+    CSV.open(path, "wb", row_sep: "\r\n") do |csv|
+      csv << Cid.attribute_names
+      scope.find_each(batch_size: 5000) do |cid|
+        csv << cid.attributes.values
+      end
+    end
+    filename = path.split('/').last
+    folders = path.split('/')[0..-2].join('/')
+    `cd #{folders} && tar -czf #{filename}.tar.gz #{filename}`
+    archive_path = "#{folders}/#{filename}.tar.gz"
+    records = scope.count
+    resp = Node.ipfs_client.add(archive_path)
+    Export.create(filename: resp['Name'],
+                  kind: 'cids',
+                  cid: resp['Hash'],
+                  size: resp['Size'],
+                  records: records,
+                  description: "CIDs with more than #{wants_count} want requests")
+    File.delete(path)
+    File.delete(archive_path)
   end
 end
